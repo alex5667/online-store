fd43d072e0afd2a3ca9b69f351f68444ac79b853
alex5667 <alex56671111@gmail.com>
2022-12-15 21:47:03 +0200

    feat: add webpack

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  "rules": {
+    "@typescript-eslint/no-explicit-any": 2,
+    '@typescript-eslint/no-var-requires': 0,
+  },
+  "env": {
+    "browser": true,
+    "amd": true,
+    "node": true
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    mode: 'development',
+    optimization: {
+        minimize: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                include: [path.resolve(__dirname, 'src')],
+                use: 'ts-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(?:|gif|png|jpg|svg)$/,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/components/img'),
+                    to: path.resolve(__dirname, 'dist/components/img')
+                }
+            ],
+        }),
+
+
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
