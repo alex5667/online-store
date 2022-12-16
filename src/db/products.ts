d11f94f1d97b7614c31fc955fdbf915a84729a24
alex5667 <alex56671111@gmail.com>
2022-12-16 15:14:47 +0200

    feat: add models file

--- src/db/products.ts
+++ src/db/products.ts
@@ -1,17 +1,5 @@
 
-export interface Product {
-  id: number,
-  title: string,
-  description: string,
-  price: number,
-  discountPercentage: number,
-  rating: number,
-  stock: number,
-  brand: string,
-  category: string,
-  thumbnail: string,
-  images: string[],
-}
+import { Product } from "../components/models/product";
 
 
 const products: Product[] = [
