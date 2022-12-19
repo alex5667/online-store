445ad53ed664cc13f05d66f57771ac85922b663a
alex5667 <alex56671111@gmail.com>
2022-12-19 18:40:49 +0200

    feat: add  state

--- src/components/Filters/Filters.ts
+++ src/components/Filters/Filters.ts
@@ -1,17 +1,16 @@
 import CheckboxItem from '../Filters/Checkbox/CheckboxItem';
 import { categories } from '../../db/productsProperties';
 import ProductsList from '../ProductsList/ProductsList';
+import {STATE_FILTER,Filter} from '../models/filter'
 export default class Filters {
-  categories: string[];
+  categories: string[]= categories;
+  state:Filter;
   constructor() {
-    this.categories = categories;
-
+    this.state =STATE_FILTER;
     this.render();
   }
   render(): void {
-
     this.chekboxFilters();
-
 
   }
   chekboxFilters(): void {
